046cd4d70c0c45e4d4605ac7866f4f35a71b08b7
Cameron Lares <cameronlares@gmail.com>
2020-10-16 09:52:45 -0700

    Completed User & Registration and was able to retreive jokes with JWT token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,81 @@
-const router = require('express').Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
+const config = require("../api/config");
 
-router.post('/register', (req, res) => {
+const Users = require("../users/users-model");
+const { isValid } = require("../users/users-service");
+
+router.post("/register", (req, res) => {
   // implement registration
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash password
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    //save the user to the databse
+
+    Users.add(credentials)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password should be alphanumeric",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+
+  const { username, password } = req.body;
+  if (isValid(req.body)) {
+    Users.findBy({
+      username,
+    }).then(([user]) => {
+      //compare the password
+      if (user && bcryptjs.compareSync(password, user.password)) {
+        const token = getJwt(user);
+
+        res.status(200).json({ message: `Welcome ${username} to the database`, token})
+      } else {
+        res.status(401).json({message: "Invalid credentials"})
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: error.message})
+    })
+  } else {
+    res.status(400).json({
+      message: " please provide username and password"
+    })
+  }
 });
+
+function getJwt(user){ //used within login to retrieve token 
+
+const payload = {
+  username: user.username,
+  // role: user.role,
+}
+
+const jwtOptions = {
+  expiresIn: "8h",
+}
+
+return jwt.sign(payload, config.jwtSecret, jwtOptions)
+
+}
+
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,27 @@
   before granting access to the next middleware/route handler
 */
 
+const e = require("express");
+const jwt = require("jsonwebtoken")
+
+const {jwtSecret} = require("../api/config")
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  console.log(token)
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        //token is invalud
+        res.status(401).json({ you: "Invalid Token"}) 
+      } else {
+        // token is valid
+        req.jwt = decodedToken
+        next();
+      }
+    })
+  } else{
+    res.status(401).json({ you: 'shall not pass!' });
+  }
+
 };
